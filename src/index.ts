767eee141d0058ec095ed0424a2ba2e2419196b0
nogo <0xnogo@proton.me>
2022-10-29 11:34:05 +0100

    first api squeleton

--- src/index.ts
+++ src/index.ts
@@ -3,45 +3,20 @@ import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 const port = 3000;
 
-interface LocationWithTimezone {
-  location: string;
-  timezoneName: string;
-  timezoneAbbr: string;
-  utcOffset: number;
-};
+app.post('/account/:clientId', (req, res) => {
+  // Get the master seed from app table
+  // Derive a private/public key pair
+  // Store it in the user table
+  // return the public key
+  res.send(req.params);
+});
 
-const dummyReponse = (request: Request, response: Response, next: NextFunction) => {
-  let locations: LocationWithTimezone[] = [
-    {
-      location: 'Germany',
-      timezoneName: 'Central European Time',
-      timezoneAbbr: 'CET',
-      utcOffset: 1
-    },
-    {
-      location: 'China',
-      timezoneName: 'China Standard Time',
-      timezoneAbbr: 'CST',
-      utcOffset: 8
-    },
-    {
-      location: 'Argentina',
-      timezoneName: 'Argentina Time',
-      timezoneAbbr: 'ART',
-      utcOffset: -3
-    },
-    {
-      location: 'Japan',
-      timezoneName: 'Japan Standard Time',
-      timezoneAbbr: 'JST',
-      utcOffset: 9
-    }
-  ];
-
-  response.status(200).json(locations);
-};
-
-app.get('/dummy', dummyReponse);
+app.get('/account/export/:userId/:targetAddress', (req, res) => {
+  // ?? targetAddress: is the userId (public key) the only thing needed?
+  // Get the private key in the user table from the userId
+  // return it
+  res.send(req.params);
+});
 
 app.listen(port, () => {
   console.log(`Lobster api is running on port ${port}.`);
