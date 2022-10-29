8cf6254feae4aac927734207ab058a2d5deac1ec
Matthew <mtwichan@gmail.com>
2022-10-29 16:50:07 +0100

    feat: add deploy nft smart contract to backend

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,34 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        process.env.PRIVATE_KEY || ""
+      ],
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/917dfa326f1f4b24afb2ec4528129870",
+      accounts: [
+        process.env.PRIVATE_KEY  || ""
+      ],
+    },
+  }
 };
 
 export default config;
