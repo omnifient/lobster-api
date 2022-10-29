468d795bce8c78fcb93d619b99f58ed5f85ccfe7
omnifi <omnifient@gmail.com>
2022-10-29 11:58:22 +0100

    comments

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from "express";
 const app = express();
 const port = 3000;
 
+// CREATE ACCOUNT
 app.post("/account/:clientId", (req, res) => {
   // Get the master seed from app table
   // Derive a private/public key pair
@@ -11,22 +12,26 @@ app.post("/account/:clientId", (req, res) => {
   res.send(req.params);
 });
 
+// DEPLOY NFT CONTRACT
 app.post("/nft/contract/:clientId", (req, res) => {
   // deploy smart contract for client_id
   // returns contract id
   res.json({ hello: "world" });
 });
 
+// MINT NFT
 app.post("/nft/collection/:collectionId", (req, res) => {
   // mint an nft of the collection
   res.json({ hello: "world" });
 });
 
+// UPDATE NFT
 app.put("/nft/collection/:collectionId/:nftId", (req, res) => {
   // update the nft of the collection (e.g. transfer nft)
   res.json({ hello: "world" });
 });
 
+// EXPORT ACCOUNT ASSETS
 app.get("/account/export/:userId/:targetAddress", (req, res) => {
   // ?? targetAddress: is the userId (public key) the only thing needed?
   // Get the private key in the user table from the userId
