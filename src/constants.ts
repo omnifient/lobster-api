1be1ff631bf835ef95e6e0f2a0eb993b893ce9ca
Matthew <mtwichan@gmail.com>
2022-10-30 10:03:35 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 export const NFT_FACTORY_ADDRESS: string = "0x796C29b3eA29c0464C055A75Ee168b4f67BC84B7"
 export const NETWORKS: object = {
-    "mumbai": "https://rpc-mumbai.maticvigil.com"
+    "mumbai": "https://rpc-mumbai.maticvigil.com",
+    "gnosis": "https://rpc.gnosischain.com",
+    "cronos": "https://evm-t3.cronos.org"
 }
